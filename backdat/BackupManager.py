9b5c066eadf4d76f5609f37c16f7c6d1e660df26
Tylar Murray <murray.tylar@gmail.com>
2017-09-15 17:33:18 +0000

    cron-like time range implemented & seems working. closes #2

--- backdat/BackupManager.py
+++ backdat/BackupManager.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+
+from croniter import croniter
+
 from backdat.RemoteInterface import rclone
 from backdat import backup_plan_parser
 from backdat import host_settings_parser
@@ -21,9 +25,9 @@ class BackupManager(object):
         """
         starts running backups until we are outside of our allotted window
         """
-        for next_backup in self.load_backup_plan():
+        for next_backup in BackupManager.load_backup_plan():
             self.set_next_backup(next_backup)
-            if (self.enough_time_remaining()):
+            if (BackupManager.enough_time_remaining()):
                 self.do_next_backup()
             else:
                 # TODO: schedule next run of BackupManger in crontab & exit
@@ -34,24 +38,52 @@ class BackupManager(object):
         """
         rclone.backup(self.next_backup_args)
 
-    def enough_time_remaining(self):
+    @staticmethod
+    def enough_time_remaining():
         """
         return true if we have enough time left in the window to complete
         the next backup task
         """
         settings = host_settings_parser.read("/etc/opt/backdat/host-settings.cfg")  # TODO: make this cross-platform
-        if settings[host_settings_parser.KEYS.BACKUP_TIMES] == "* 0-8 * * *":
-            # TODO: if time left in window
-            return True
-        else:
-            return False
+        estimated_next_backup_tf = datetime.now() + timedelta(minutes=5)  # TODO: calc better estimate
+        return BackupManager.inside_cron_window(
+            estimated_next_backup_tf,
+            settings[host_settings_parser.KEYS.BACKUP_TIMES]
+        )
+
+    @staticmethod
+    def inside_cron_window(dtime, windowstr):
+        """
+        returns true if given datetime is within given widow cron string
+
+        ---------
+        dtime : datetime
+        windowstr : str
+        """
+        # threshold should be << than window width, but > cron granularity
+        # this method assumes that the cron string minutes column is *, ie
+        # cron granularity is 1min and min window width is 1hr.
+        threshold = timedelta(minutes=2)
+
+        window_iter = croniter(windowstr, dtime)
+        while(True):
+            next_time = window_iter.get_next(datetime)
+            if next_time < dtime:
+                pass  # keep getting times until we pass our target time
+            elif next_time - dtime < threshold:
+                # if the next time is close to our target time
+                return True
+            else:
+                # if the next time is far off, we probably exited our window
+                return False
 
     def set_next_backup(self, backup_dict):
         """ loads given backup dict into next_backup_args """
         self.next_backup_args.source = backup_dict["source"]
         self.next_backup_args.target = backup_dict["target"]
 
-    def load_backup_plan(self):
+    @staticmethod
+    def load_backup_plan():
         """
         loads backup plan actions for this host from file into a list
         """

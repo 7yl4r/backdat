bed455b310a31e0cee296ed73ddfdaf3218239b8
Tylar Murray <murray.tylar@gmail.com>
2017-11-14 16:15:33 +0000

    handle no logs yet, shift year of datetime.min to work in py3.4

--- backdat/file_parsers/backup_history.py
+++ backdat/file_parsers/backup_history.py
@@ -3,6 +3,7 @@ This user-facing file is a history of items that have been backed up.
 """
 
 from datetime import datetime
+import logging
 
 from backdat.file_parsers.fileset import get_fileset_statlist, STAT_KEYS
 
@@ -40,17 +41,34 @@ def get_most_stale_file(logpath=DEFAULT_PATH):
             oldest_name = fstat[STAT_KEYS.SOURCE]
     return oldest_name, oldest_date
 
+def get_dawn_of_time():
+    """
+    returns the earliest possible time we can handle as formatted string.
+
+    NOTE: We're not using datetime.min directly because strptime doesn't like
+    years shorter than 4 digits and datetime.min.year == 1 :
+        # tested in python 3.4:
+        >>> datetime.strptime(datetime.strftime(datetime.min, TIME_FORMAT), TIME_FORMAT)
+        ValueError: time data '1-01-01 00:00' does not match format '%Y-%m-%d %H:%M'
+    """
+    return datetime.strftime(datetime.min.replace(year=1000), TIME_FORMAT)
+
 def get_last_upload_time(filename, logpath=DEFAULT_PATH):
     """
     returns the last time given filename was uploaded,
     assumes backup file has cronological entries.
     assumes we can fit the whole backup log into memory.
     """
-    with open(logpath, 'r') as logfile:
-        for line in reversed(list(logfile.readlines())):
-            time, src, path = line.split("\t")
-            if src == filename:
-                return time
-        else:
-            # file has never been backed up, return the dawn of time
-            return str(datetime.min)
+    logger = logging.getLogger(__name__)
+    try:
+        with open(logpath, 'r') as logfile:
+            for line in reversed(list(logfile.readlines())):
+                time, src, path = line.split("\t")
+                if src == filename:
+                    return time
+            else:
+                # file has never been backed up, return the dawn of time
+                return get_dawn_of_time()
+    except FileNotFoundError as f_err:
+        logger.warn("No backup history found")
+        return get_dawn_of_time()

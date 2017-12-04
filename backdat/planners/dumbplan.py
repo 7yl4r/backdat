e78ffc7c9e7ecd4a66406b6b8fa3c11442710b41
Tylar Murray <murray.tylar@gmail.com>
2017-12-04 21:30:55 +0000

    doh! fix overzealous path trim from 62416a9a15b74736b96cd117470c35607d834363

--- backdat/planners/dumbplan.py
+++ backdat/planners/dumbplan.py
@@ -27,7 +27,7 @@ def make_plan():
             timestr = "2222-22-22T22:22:22"
             src = hostname +":" + fstat[STAT_KEYS.SOURCE]
 
-            trg_dir = '/'.join(fstat[STAT_KEYS.SOURCE].split('/')[:-2])
+            trg_dir = '/'.join(fstat[STAT_KEYS.SOURCE].split('/')[:-1])
             trg = (  # TODO: replace hardcoded target with read from resources.json
                 "gdrive-ty:/IMARS/backups/backdat/" +
                 hostname +

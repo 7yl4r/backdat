0186bc5432f8c00af8d4cb910d9c3c4ceb3fb8ae
Tylar Murray <murray.tylar@gmail.com>
2018-01-18 21:29:57 +0000

    use get on dicts to give default value for backward-compat

--- backdat/file_parsers/backup_stats.py
+++ backdat/file_parsers/backup_stats.py
@@ -27,12 +27,15 @@ def update_stats(completed_action, action_status, filepath=DEFAULT_STATS_PATH):
     """
     stats = read()
     if action_status == ACTION_STATUS.DONE:
-        stats[KEYS.ACTIONS_COMPLETED] += 1
+        stats[KEYS.ACTIONS_COMPLETED] = stats.get(KEYS.ACTIONS_COMPLETED, 0) + 1
         # NOTE: this does not account for files that were only checked and not
         # actually uploaded. Thus `uploaded_bytes` is probably larger than actual throughput.
-        stats[KEYS.BYTES_UPLOADED] += os.stat(completed_action.source).st_size
+        stats[KEYS.BYTES_UPLOADED] = (
+            stats.get(KEYS.BYTES_UPLOADED, 0) +
+            os.stat(completed_action.source).st_size
+        )
     elif action_status == ACTION_STATUS.FAIL:
-        stats[KEYS.ACTIONS_FAILED] += 1
+        stats[KEYS.ACTIONS_FAILED] = stats.get(KEYS.ACTIONS_FAILED, 0) + 1
     else:
         raise ValueError("action status not recognized: " + str(action_status))
     write(stats)

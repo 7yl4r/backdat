451d1893b132209c3d38d18d621f3ce4d165aedb
Tylar Murray <murray.tylar@gmail.com>
2018-09-27 21:07:17 +0000

    + max_period to metrics

--- metrics.sh
+++ metrics.sh
@@ -3,6 +3,7 @@
 # collection of quick metrics for monitoring collectors like telegraf
 printf "{\n"
 printf "\t\"backdat.plan_len\": `cat /var/opt/backdat/backup-plan.tsv | wc -l`,\n"
+printf "\t\"backdat.max_period\": `sudo ./backdat.py status --max_period`,\n"
 # this next part is a bit hackish...
 cat /var/opt/backdat/backup-stats.json | tr -d '{}'
 

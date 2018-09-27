5746f4d16d7b10faa844b63c5cb1b7c233241738
Tylar Murray <murray.tylar@gmail.com>
2018-09-27 21:26:56 +0000

    real fix path to backdat

--- metrics.sh
+++ metrics.sh
@@ -3,7 +3,7 @@
 # collection of quick metrics for monitoring collectors like telegraf
 printf "{\n"
 printf "\t\"backdat.plan_len\": `cat /var/opt/backdat/backup-plan.tsv | wc -l`,\n"
-printf "\t\"backdat.max_period\": `sudo opt/backdat/backdat.py status --max_period`,\n"
+printf "\t\"backdat.max_period\": `sudo /opt/backdat/backdat.py status --max_period`,\n"
 # this next part is a bit hackish...
 cat /var/opt/backdat/backup-stats.json | tr -d '{}'
 
